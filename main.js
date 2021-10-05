e5b0746c571aa8481584fdc194421cf2301faa9d
Eugene Trotsenko <evenguy@yandex.ru>
2021-10-05 16:11:23 +0600

    Added getters and setters

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const COUNTER_UPDATE_TIMEOUT = 500;
-const HEAD_TITLE_POSTFIX = ' - workTime';
+const HEAD_TITLE_POSTFIX = ' - Time';
 
 const HTML_QUERY_END_TIME = '#end-time';
 const HTML_QUERY_TITLE = 'head title';
@@ -55,12 +55,12 @@ function getTargetTime( timeText )
 
             if ( validateTime( timeElements[0], timeElements[1] ) )
             {
-                return new WorkTime( timeElements[0], timeElements[1] );
+                return new Time( timeElements[0], timeElements[1] );
             }
         }
     }
 
-    return new WorkTime();
+    return new Time();
 }
 
 function validateTime( hours, minutes )
@@ -82,7 +82,7 @@ function getAvailableTime( endTime )
 {
     if ( ( endTime.hours == undefined ) || ( endTime.minutes == undefined ) )
     {
-        return new WorkTime();
+        return new Time();
     }
 
     let currentDateTime = moment();
@@ -92,7 +92,7 @@ function getAvailableTime( endTime )
 
     let diffTime =  moment.duration( endDateTime.diff( currentDateTime ) );
 
-    return new WorkTime( 
+    return new Time( 
         diffTime.hours(),
         diffTime.minutes(),
         diffTime.seconds()
@@ -110,9 +110,9 @@ function updateCounterUI( time )
 
 function updateCounterTimerUI( field, time )
 {
-    field.querySelector( '.hours' ).innerText = time.elementToText( time.hours );
-    field.querySelector( '.minutes' ).innerText = time.elementToText( time.minutes );
-    field.querySelector( '.seconds' ).innerText = time.elementToText( time.seconds );
+    field.querySelector( '.hours' ).innerText = time.hoursText;
+    field.querySelector( '.minutes' ).innerText = time.minutesText;
+    field.querySelector( '.seconds' ).innerText = time.secondsText;
 }
 
 function updateCounterTitleTUI( field, time )
@@ -147,13 +147,14 @@ function getLocalTime()
     let localTime = localStorage.getItem( LOCAL_STORAGE_END_TIME_KEY );
     if ( ( localTime == null ) || ( localTime == undefined ) )
     {
-        return new WorkTime();
+        return new Time();
     }
     
     return getTargetTime( localTime );
 }
 
-function WorkTime( hours = undefined, minutes = undefined, seconds = undefined )
+// Time object constructor
+function Time( hours = undefined, minutes = undefined, seconds = undefined )
 {
     this.elementToText = function ( time )
     {
@@ -200,7 +201,64 @@ function WorkTime( hours = undefined, minutes = undefined, seconds = undefined )
         return this.elementToText( this.hours ) + ':' + this.elementToText( this.minutes );
     }
 
-    this.hours = this.correctTime( hours );
-    this.minutes = this.correctTime( minutes );
-    this.seconds = this.correctTime( seconds );
+    Object.defineProperty( this, 'hours', {
+        get()
+        {
+            return this._hours;
+        },
+
+        set( value )
+        {
+            this._hours = this.correctTime( value );
+        }
+    });
+
+    Object.defineProperty( this, 'minutes', {
+        get()
+        {
+            return this._minutes;
+        },
+
+        set( value )
+        {
+            this._minutes = this.correctTime( value );
+        }
+    });
+
+    Object.defineProperty( this, 'seconds', {
+        get()
+        {
+            return this._seconds;
+        },
+
+        set( value )
+        {
+            this._seconds = this.correctTime( value );
+        }
+    });
+
+    Object.defineProperty( this, 'hoursText', {
+        get()
+        {
+            return this.elementToText( hours );
+        }
+    });
+
+    Object.defineProperty( this, 'minutesText', {
+        get()
+        {
+            return this.elementToText( minutes );
+        }
+    });
+
+    Object.defineProperty( this, 'secondsText', {
+        get()
+        {
+            return this.elementToText( seconds );
+        }
+    });
+
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
 }
